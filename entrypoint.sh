db024f950863151a780624b19736cb3a057192bd
Gayane Arshakyan <gayane.arshakyan@project-a.com>
2020-03-13 13:00:00 +0100

    validator

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
-name: 'Hello World'
-description: 'Greet someone and record the time'
-inputs:
-  who-to-greet:  # id of input
-    description: 'Who to greet'
-    required: true
-    default: 'World'
-outputs:
-  time: # id of output
-    description: 'The time we greeted you'
-runs:
-  using: 'docker'
-  image: 'Dockerfile'
-  args:
-    - ${{ inputs.who-to-greet }}
+#!/bin/sh -l
+
+echo "Start validation $1"
+
+
+echo ::set-env name=BRANCH::$(git branch -r --contains ${GITHUB_SHA} | grep "")
+echo ::set-env name=RELEASE_VERSION::$(echo ${GITHUB_REF} | sed -e "s/refs\/tags\///g" | sed -e "s/\//-/g")
+
+MASTER_BRANCH_NAME='origin/master'
+RELEASE_PREFIX='r-'
+
+if [[ "${{ env.BRANCH }}" == *"$MASTER_BRANCH_NAME"* ]] && [[ "${{ env.RELEASE_VERSION }}" == "$RELEASE_PREFIX"* ]]; then
+  echo ::set-output name=validation_response::"Release tag validation succeeded!"
+  exit 0
+else
+  echo ::set-output name=validation_response::"Release tag validation failed!"
+  exit 1
+fi
